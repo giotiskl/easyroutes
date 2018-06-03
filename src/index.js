db3dd043d6bd1458b210bb20c277824f11cfa828
Yiotis Kaltsikis <giotisgr@gmail.com>
2018-06-03 18:15:05 +0200

    Remove debug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import EasyRoutes from './easyroutes';
 
 export default EasyRoutes;
-
-window.easy = new EasyRoutes();
-easy({
-  consultant: '/consultant/:id',
-  projects: { startProject: '/start/project/:id' },
-});
